c18769253b1d6f389ef5e5ee5f19699a1ff07e53
Ron van Hezik <ron@hezik.nl>
2025-02-28 10:37:11 +0100

    ci

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 cd siaalert
 go mod tidy
-docker buildx build --platform linux/amd64,linux/arm64 --tag back2basic/siaalert:latest -f ./docker/Dockerfile --push .
+docker buildx build --platform linux/amd64,linux/arm64 --tag back2basic/siaalert:latest -f ./docker/Dockerfile .
 cd ..
