36e8178569ade2842c801bc33ca98ad81aa8c739
Ron van Hezik <ron@hezik.nl>
2025-03-02 22:40:15 +0100

    cleanup

--- siaalert/cron/queue.go
+++ siaalert/cron/queue.go
@@ -109,26 +109,6 @@ func worker(id int, tasks <-chan Job, wg *sync.WaitGroup, checker scan.Checker) 
 			}
 			sdk.CheckUpdateStatus(job.Name, job.Address, "", true)
 			checker.PortScan(job.Name, scanned, &sdkWg, sdkQueue)
-			// scanned, err := bench.ScanHosts(job.Address, job.HostKey)
-			// if err != nil {
-			// 	sdk.CheckUpdateStatus(job.Name, job.Address, err.Error(), false)
-			// 	_, port, err := checker.SplitAddressPort(job.Address)
-			// 	if err != nil {
-			// 		fmt.Println(err)
-			// 	}
-			// 	// convert string to int add 1 to port and change back to string
-			// 	mux, err := strconv.Atoi(port)
-			// 	if err != nil {
-			// 		fmt.Println(err)
-			// 	}
-			// 	mux++
-			// 	port = strconv.Itoa(mux)
-			// 	scan := bench.Scan{Settings: bench.Settings{Acceptingcontracts: false, Netaddress: job.Address, Siamuxport: port}, PriceTable: bench.PriceTable{}}
-			// 	checker.PortScan(job.Name, scan)
-			// } else {
-			// 	sdk.CheckUpdateStatus(job.Name, job.Address, "", true)
-			// 	checker.PortScan(job.Name, scanned)
-			// }
 		default:
 			fmt.Printf("WIP %d: Unknown job: %s\n", id, job.Address)
 		}
